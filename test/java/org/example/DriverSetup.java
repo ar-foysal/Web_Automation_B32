c55a7138f7c7312fe2141cd2552ca31e6026df3a
MD ABDUR RAHMAN <45840084+arfoysal@users.noreply.github.com>
2025-08-01 22:16:18 +0600

    Add manage browser actions

---
+++ test/java/org/example/DriverSetup.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class DriverSetup {
+    WebDriver browser;
+
+    @BeforeSuite
+    public void openBrowser(){
+        browser =new ChromeDriver();
+    }
+
+    @AfterSuite
+    public void closeBrowser(){
+//        browser.close();
+        browser.quit();
+    }
+}
